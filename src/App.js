d5936533c5ee3d9fc8a64be9b504c733bd932152
jonahlage <jonahxray@gmail.com>
2024-10-27 17:14:04 -0700

    updated code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Maintenance in Progress</h1>
+                <p>
+                    Jonah's portfolio site will be live soon! Stay tuned for updates.
+                </p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
+
